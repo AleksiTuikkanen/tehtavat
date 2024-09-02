04c7cbd3f80280257feaa350a3b063c3ecbb5b3b
AleksiTuikkanen <aleksi.tuikkanen@metropolia.fi>
2024-09-02 14:57:53 +0300

    Ohjelmisto 1

--- Moduuli 5/Moduuli 5.2.py
+++ Moduuli 5/Moduuli 5.2.py
@@ -1,7 +1,7 @@
-luvut = []
+luvut = [] # Luodaan tyhjä lista
 luku=input("Anna luku tai paina enteriä lopettaaksesi: ")
-while luku!="":#jos luku on eri kuin tyhjä toistetaan
+while luku!="": # Jos luku on eri kuin tyhjä toistetaan
     luvut.append(int(luku))
     luku = input("Anna luku tai paina enteriä lopettaaksesi: ")
-luvut.sort(reverse=True)
+luvut.sort(reverse=True) # Lajitellaan luvut suurimmasta pienimpään
 print(luvut)
