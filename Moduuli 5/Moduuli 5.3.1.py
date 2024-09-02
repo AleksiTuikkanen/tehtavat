ed8b93e93a457346dd7b489d4cb91e134061091c
AleksiTuikkanen <aleksi.tuikkanen@metropolia.fi>
2024-09-02 15:04:21 +0300

    Ohjelmisto 1

--- Moduuli 5/Moduuli 5.3.1.py
+++ Moduuli 5/Moduuli 5.3.1.py
@@ -1,8 +1,8 @@
 #Tein tehtävän myös "väärin" eli toteutin saman eri funktioilla
 
 luku = int(input("Anna kokonaisluku:"))
-jakaja = 1 # Määritellään jakajaksi 1
-while luku % jakaja !=0 and jakaja == luku: # Niin kauan kuin jakojäännös ei ole 0 JA jakaja ei ole sama kuin luku
+jakaja = 2 # Määritellään jakajaksi 1
+while luku % jakaja !=0 and jakaja != luku: # Niin kauan kuin jakojäännös ei ole 0 JA jakaja ei ole sama luku
     jakaja += 1 # lisätään jakajaan 1
 if jakaja == luku: # Jos toistorakenne sulkeutuu siksi, että jakaja on sama kuin luku kyseessä on alkuluku
     print(f"{luku} on alkuluku.")
